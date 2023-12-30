0caca0f3a3b65a56c5976506fa4582cf1f9d3ba8
mvfernando <eu@mvfernando.rf.gd>
2023-12-30 04:36:39 +0100

    add user models and config api

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:santander_app/pages/services/api.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    fetch();
+  }
+
+  fetch() async {
+    var user = await ApiApp.fetchUser(1);
+    print(user);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
